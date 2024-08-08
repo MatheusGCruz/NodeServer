8a7fa6f39d4cc49dde03b2435c07c0a7e7711ebd
MatheusGCruz <matheuskaze@gmail.com>
2024-08-08 07:39:04 -0300

    Added music server

--- index.js
+++ index.js
@@ -85,21 +85,67 @@ app.get('/mkv-videos/:filename', ( req, res)=>{
     }
 })
 
-app.get('/files', ( req, res)=>{
+app.get('/videoFiles', ( req, res)=>{
     const head = {
         'Content-Type':'application/json',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers':'X-Requested-With'
     };
-
     const files = fs.readdirSync('videos/');
-
-    // search through the files
-
     res.setHeader('Content-Type','application/json');
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Headers','X-Requested-With');
     return res.send(files);
+})
+
+app.get('/musicFiles', ( req, res)=>{
+    const head = {
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':'X-Requested-With'
+    };
+    const files = fs.readdirSync('music/');
+    res.setHeader('Content-Type','application/json');
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','X-Requested-With');
+    return res.send(files);
+})
+
+app.get('/music/:filename', ( req, res)=>{
+    const filename = req.params.filename;
+    const filePath = "music/"+[filename];
+    if(!filePath){
+        return res.status(404).send('Not Found')
+    }
+
+    const stat = fs.statSync(filePath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if(range){
+        const parts = range.replace(/bytes=/,'').split('-')
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10): fileSize - 1;
+
+        const chunkSize = end - start +1;
+        const file = fs.createReadStream(filePath, {start, end});
+        const head = {
+            'Content-Range': 'bytes '+start+'-'+end+'/'+fileSize,
+            'Accept-Ranges': 'bytes',
+            'Content-Length':chunkSize,
+            'Content-Type':'audio/mpeg3'
+        }
+        res.writeHead(206, head);
+        file.pipe(res);
+    }
+    else{
+        const head = {
+            'Content-Length':fileSize,
+            'Content-Type':'audio/mpeg3'
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(filePath).pipe(res);
+    }
 })
 
 app.listen(3005, ()=>{
