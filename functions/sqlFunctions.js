ae4be57990856a207b7d0b40de25a1f57bd12480
Matheus - Antares <matheus@antares.ninja>
2025-01-31 19:08:51 -0300

    hotfix 1

--- functions/sqlFunctions.js
+++ functions/sqlFunctions.js
@@ -9,17 +9,27 @@ module.exports = {
 
         const command = "SELECT vbookContent FROM [NODE_SERVER].[dbo].[VBOOK] where vbookName = '"+req.params.bookname+"'";
 
-        //const searched = (await dataAccess.query(command)).recordSet;
-
         const bookContent = (await req.app.locals.db.vbook.query(command)).recordset;
         
-        //console.log(searched);
-        console.log(bookContent);
         if(bookContent[0] && bookContent[0].vbookContent){
             return bookContent[0].vbookContent;
         }
         return "Error when searching for book";
         
-    }    
+    }  , 
+    
+    searchCover: async function (req){
+        console.log("book:"+ req.params.bookname);
+
+        const command = "SELECT vbookCoverBase64 FROM [NODE_SERVER].[dbo].[VBOOK] where vbookName = '"+req.params.bookname+"'";
+
+        const bookContent = (await req.app.locals.db.vbook.query(command)).recordset;
+
+        if(bookContent[0] && bookContent[0].vbookContent){
+            return bookContent[0].vbookContent;
+        }
+        return "Error when searching for book";
+        
+    } 
 }
 
